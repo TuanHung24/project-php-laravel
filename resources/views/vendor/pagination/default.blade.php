b38548e6f6046717f3dca925f5d336d596308a4c
TuanHung24 <tuanhung2401003@gmail.com>
2024-01-16 15:11:03 +0700

    HungUp

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,3 +1,31 @@
+<style>
+.pagination-link, .pagination-disabled {
+    padding: 8px 16px; /* Điều chỉnh kích thước padding */
+    margin: 4px;
+    border: 1px solid #ddd; /* Đường viền */
+    border-radius: 4px; /* Bo góc */
+    text-decoration: none; /* Loại bỏ gạch chân */
+    color: #333; /* Màu chữ */
+    background-color: #f8f8f8; /* Màu nền */
+}
+
+.pagination-link:hover {
+    background-color: #e9e9e9; /* Màu nền khi hover */
+}
+
+.pagination-disabled {
+    color: #aaa; /* Màu chữ cho phần không hoạt động */
+    cursor: not-allowed; /* Con trỏ */
+}
+
+.pagination-info {
+    padding: 8px 16px; /* Điều chỉnh kích thước padding */
+    margin: 4px;
+    color: #333; /* Màu chữ */
+}
+</style>
+
+
 @if ($paginator->hasPages())
 <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="pagination-nav">
    
